7e2fa191486f6944e5212af97e030d78a3cdaf5a
Soumyanil Das <dassic@outlook.com>
2023-08-09 12:19:49 +0530

    created - struct Load_balancer and interface Serve

--- main.go
+++ main.go
@@ -2,27 +2,47 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 )
 
-type server struct{
-	address string
-	proxy *httputil.ReverseProxy
+type Serve interface{
+	Address() string
+	Is_alive()	bool
+	Serve(rw http.ResponseWriter, r *http.Request)
 }
 
-func new_server(address string) *server{
-	server_url, err := url.Parse(address)
+type Server struct{
+	address 	string
+	proxy 		*httputil.ReverseProxy
+}
 
+type Load_Balancer struct{
+	port 				string
+	round_robin_count 	int
+	servers 			[]Server
+}
+
+func handle_error(err error){
 	if err != nil {
 		panic(err);
 	}
+}
 
-	return &server{
+func new_server(address string) *Server{
+	server_url, err := url.Parse(address)
+
+	handle_error(err);
+
+	return &Server{
 		address: address,
 		proxy: httputil.NewSingleHostReverseProxy(server_url),
 	}
 }
+
+
+
 
 func main(){
 	fmt.Println("Hello");
